d6dbb37f48a92f02ecf9f8a3d86941a4b13accee
NunesAzevedo <pnazevedo1@gmail.com>
2024-11-21 17:13:09 -0300

    Add instalations of the repositories with bash's alias and oh my posh themes

---
+++ install_personalizations.sh
@@ -0,0 +1,13 @@
+# Instalations in Fedora 41
+cd ~
+# Oh my posh themes
+echo -e "\n*********************************"
+echo -e "\nInstalando os temas do Oh my posh"
+echo -e "\n*********************************\n"
+git clone git@github.com:NunesAzevedo/oh_my_posh_themes.git
+
+# Personal bash alias
+echo -e "\n***************************************"
+echo -e "\nInstalando Alias do bash personalizadas"
+echo -e "\n***************************************\n"
+git clone git@github.com:NunesAzevedo/.bashrc.d.git
